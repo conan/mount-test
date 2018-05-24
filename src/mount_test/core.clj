a314e2a7639dce2d5eecd3cb795bc4e7fef86929
conan <conan@joinef.com>
2018-05-24 15:15:56 +0100

    single namespace

--- src/mount_test/core.clj
+++ src/mount_test/core.clj
@@ -1,6 +1,24 @@
 (ns mount-test.core
   (:require [clj-http.client :as http]
-            [mount-test.web :as web]
-            [mount.core :as mount]))
+            [mount.core :as mount]
+            [org.httpkit.server :as server]
+            [reitit.ring :as rr]))
+
+(defn app
+  []
+  (rr/ring-handler
+    (rr/router
+      ["/" {:get (constantly {:status 200 :body "hi there"})}])
+    (rr/create-default-handler)))
+
+(defn start
+  [port]
+  (server/run-server (app) port))
+
+(mount/defstate web-server
+  :start (start 4321)
+  :stop (web-server))
 
 (prn "Starting" (mount/start))
+(prn "HTTP request:")
+(prn (http/get "http://localhost:4321"))
